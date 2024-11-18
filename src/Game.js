2c15b634c110e089950659beb5fde155ae89bfec
Muktar Ali <muktarali1004@gmail.com>
2024-11-18 16:29:19 -0500

    Initial commit

---
+++ src/Game.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import GameGrid from "./GameGrid.js";
+import './styles.css';
+
+// TODO: Import useState() hook
+
+function Game() {
+
+   // TODO: Replace variables with state variables
+   const [moves, setMoves] = useState(new Array(9).fill(""));
+   const [turn, setTurn] = useState("X");
+  
+   function gridClick(whichSquare) {
+      // TODO: Replace with code to set the move and turn
+      if (moves[whichSquare] === "") {
+         const movesCopy = moves.slice();
+         movesCopy[whichSquare] = turn; 
+         setMoves(movesCopy);
+
+         if (turn === "X") {
+            setTurn("O");
+          } else {
+            setTurn("X");
+          }
+   }
+}
+
+   // TODO: Add newGame() function here
+   function newGame() {
+      setMoves(new Array(9).fill(""));
+      setTurn("X");
+   }
+
+   // TODO: Make New Game button to call newGame() when clicked
+   return (
+      <>
+         <h1>Tic-Tac-Toe</h1>        
+         <GameGrid moves={moves} click={gridClick} />
+         <p>
+            Turn: <strong className={turn}>{turn}</strong>
+         </p>
+         <p>
+         <button onClick={newGame}>New Game</button>
+         </p>
+      </>
+   );
+}
+
+export default Game;
